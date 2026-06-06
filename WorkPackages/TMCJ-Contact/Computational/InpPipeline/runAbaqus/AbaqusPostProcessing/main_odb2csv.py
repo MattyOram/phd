97d4087073e03d8da9f2f13bd57ff3f1b75fe0c1
Matty <mattheworam1@outlook.com>
2026-06-06 13:29:18 +0100

    now defaults to exporting final frame of all steps

--- WorkPackages/TMCJ-Contact/Computational/InpPipeline/runAbaqus/AbaqusPostProcessing/main_odb2csv.py
+++ WorkPackages/TMCJ-Contact/Computational/InpPipeline/runAbaqus/AbaqusPostProcessing/main_odb2csv.py
@@ -3,6 +3,16 @@ from odbHistory2csv import odbHistory2csv
 import os
 import sys
 from abaqusConstants import NODAL, INTEGRATION_POINT, ELEMENT_NODAL, ELEMENT_CENTROID
+from odbAccess import openOdb
+
+def get_step_idx_range(odb_path):
+    odb = openOdb(odb_path, readOnly=True)
+    try:
+        n_steps = len(odb.steps)
+    finally:
+        odb.close()
+
+    return range(n_steps)
 
 #odb_path = "testing/test8V2/test.odb"
 odb_path = sys.argv[1]
@@ -10,27 +20,31 @@ odb_path = sys.argv[1]
 out_dir = os.path.join(os.path.dirname(odb_path), "resultCSVs")
 os.makedirs(out_dir, exist_ok=True)
 
-step_list = [0]
+step_list = None # None to export all, list like [0, 2, -1] to export specific steps
 frame_list = [-1]
 instance_list = ["TPM_INST", "MC1_INST"]
 
+# Get step idxs if step_list == None
+if step_list is None:
+    step_list = get_step_idx_range(odb_path)
+
 field_list = [
-    "CNAREA",
+    #"CNAREA",
     "CPRESS",
-    "CSTATUS",
+    #"CSTATUS",
     "U",
     #"VF",
-    "S",
-    "LE"
+    #"S",
+    #"LE"
 ]
 position_list = [
-    NODAL,
-    NODAL,
-    NODAL,
+    #NODAL,
     NODAL,
     #NODAL,
-    INTEGRATION_POINT,
-    INTEGRATION_POINT
+    NODAL,
+    #NODAL,
+    #INTEGRATION_POINT,
+    #INTEGRATION_POINT
 ]
 
 for step_idx_s in step_list:
