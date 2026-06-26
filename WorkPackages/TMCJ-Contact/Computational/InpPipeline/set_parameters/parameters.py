d40de621d0dec5e7c5d6d13ae2dc4eec945a5296
Matty <mattheworam1@outlook.com>
2026-06-26 20:55:15 +0100

    .

--- WorkPackages/TMCJ-Contact/Computational/InpPipeline/set_parameters/parameters.py
+++ WorkPackages/TMCJ-Contact/Computational/InpPipeline/set_parameters/parameters.py
@@ -14,7 +14,8 @@ params = {
 params_gen = params['general']
 
 # root directory for outputs and save loc of params file - if relative will be relative to your current directory!
-params_gen['mesh_root']    = '../MeshPipeline/outputs/initialFEAstuff/35T/35Tbest' # output_root in MeshPipeline
+#params_gen['mesh_root']    = '../MeshPipeline/outputs/initialFEAstuff/35T/35Tbest' # output_root in MeshPipeline
+params_gen['mesh_root']    = '../MeshPipeline/outputs/initialFEAstuff/15T3T/15T3Tbest'
 #params_gen['mesh_root']    = 'outputs/element_count'
 #params_gen['mesh_root']    = '../../../../Computational/MeshPipeline/outputs/initialFEAstuff/35T/35Tbest'
 
@@ -24,7 +25,8 @@ params_gen['mesh_root']    = '../MeshPipeline/outputs/initialFEAstuff/35T/35Tbes
 #params_gen['subjects'] = ['50000R', '50017L', '50034R']  # Contact area         
 params_gen['subjects'] = ['14548R']
 
-params_gen['output_root']  = 'outputs/initialFEAstuff/accuracy/study3_35T4d5_Fsteps'  # output dir for input files and meshes        # -------- *** -------- #
+params_gen['output_root']  = 'outputs/initialFEAstuff/accuracy/study3_15T10d6'  # output dir for input files and meshes        # -------- *** -------- #
+#params_gen['output_root']  = 'outputs/initialFEAstuff/robustness/update_35T4d5_Fsteps'
 #params_gen['output_root']  = 'outputs/testing/ogden'
 
 params_gen['timeout'] = 1200 # (s) per inp time limit just in case
@@ -64,7 +66,7 @@ params_inp['mc1_patch_params'] = ("euclidean", 6) # distance of BC patch from ca
 #params_inp['element_order'] = 'quad' # 'linear' (4 node) or 'quad' (10 node (~8x linear node count))
 
 # ELEMENT TYPES
-params_inp['element_type']      = ["C3D4"] # M-Modified ARE INCOMPATIBLE WITH THE REGULAR TETRAHEDRAL ELEMENTS IF THEY SHARE THE SAME NODES
+params_inp['element_type']      = ["C3D10"] # M-Modified ARE INCOMPATIBLE WITH THE REGULAR TETRAHEDRAL ELEMENTS IF THEY SHARE THE SAME NODES
 params_inp['cartilage_element_suffix']  = ['H'] # e.g. H for C3D10H
 
 # BONE PROPERTIES
--- plan.md
+++ plan.md
@@ -1,28 +1,28 @@
-PLAN FEA SENSITIVITY STUDY
-
 
 ### d0 study
  - Redo d0 study for 3.5T4 for SMT cartilage
  - Then based on those determine minimum, then see if it works for all subs
 
-### robustness study - done for 35T4-d5 
- - generate meshes for the 3 contact patch subjects for both bones
- - generate input files for flexion, extension, abduction, adduction, pinch_load
- - Run all input files
-
-### Accuracy study for 14548neutral35T-d5 
- - Compare current setup with experimental results
- - Do FE param sensitivity study for this subject (try elastico material models that have different behaviour in tension)
- - Make the results match
-
-### Aire
-
-
-
-
-
-
 ### Change mc1 position logic
  - make position_cylinder_mc1() function
      - can then easily use on guide to visually verify guide suitability for each subject.
-     - 10mm between top of cyclinder and tip of mc1 cartilage
+ - whats stopping me from just placing the guide slightly further back so the contact is more central?
+    - Add bigger cylinder or extension platform?
+
+
+### Jobs
+- (finalise mesh d0, write script to postprocess aire, write up vero testing results, update poster)
+
+email rhys
+    - should maybe check sensor position
+update poster
+look at robertos stuff
+prepare for meeting with ruth - check meeting notes
+
+
+### Random
+- MAYBE DO PROPER TESTS WITH MULTIPLE SENSORS TO SEE IF IT EFFECTS RESULTS AND MIGHT HELP JUSTIFY NOT equilibrating
+
+- 15006 has dodgy jar_load transforms
+
+- iteravtive solver for memory issues?
